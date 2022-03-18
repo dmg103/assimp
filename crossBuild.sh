2a8468b7a274b73974d78fa0d5410ada66a89a2a
David Martinez <dmg103@alu.ua.es>
2022-03-18 11:49:22 +0100

    [master]: Commented the cross build for windows, but the problem is on the environment variables of mingw, it needs to be properly set up

--- crossBuild.sh
+++ crossBuild.sh
@@ -15,14 +15,14 @@ cp -r ./build/lib/* ./lib/
 mv ./lib/libassimp.a ./lib/libassimpLinux.a
 cp ./build/include/assimp/config.h ./include/assimp/
 
-rm -r build/
-
-#Windows
-mkdir build
-cd build
-cmake -D CMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ .. -DBUILD_SHARED_LIBS=OFF
-make -j
-cd ..
-cp -r ./build/lib/* ./lib/
-mv ./lib/libassimp.a ./lib/libassimpWin.a
-cp ./build/include/assimp/config.h ./include/assimp/
+#rm -r build/
+#
+##Windows
+#mkdir build
+#cd build
+#cmake -D CMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ .. -DBUILD_SHARED_LIBS=OFF
+#make -j
+#cd ..
+#cp -r ./build/lib/* ./lib/
+#mv ./lib/libassimp.a ./lib/libassimpWin.a
+#cp ./build/include/assimp/config.h ./include/assimp/
