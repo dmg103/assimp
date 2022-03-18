a19be4f4e3627497f8fabb6b51dabababd5de33f
David Martinez <dmg103@alu.ua.es>
2022-03-18 14:34:26 +0100

    [master]: CrossBuild fixed zlib error

--- crossBuild.sh
+++ crossBuild.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 git checkout master
 
-patch -ulbf code/Common/ZipArchiveIOSystem.cpp << EOF
-@@ -198,3 +198,5 @@
-     mapping.zopen_file = (open_file_func)open;
-+#ifdef ZOPENDISK64
-     mapping.zopendisk_file = (opendisk_file_func)opendisk;
-+#endif
-     mapping.zread_file = (read_file_func)read;
-EOF
-
 rm -r build/
 rm -r lib/
 
@@ -17,21 +8,9 @@ rm -r lib/
 mkdir build
 mkdir lib
 cd build
-cmake .. -DBUILD_SHARED_LIBS=OFF
+cmake .. -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_ZLIB=ON -DASSIMP_BUILD_ASSIMP_TOOLS=OFF -DASSIMP_BUILD_TESTS=OFF
 make -j4
 cd ..
 cp -r ./build/lib/* ./lib/
 mv ./lib/libassimp.a ./lib/libassimpLinux.a
 cp ./build/include/assimp/config.h ./include/assimp/
-
-#rm -r build/
-#
-##Windows
-#mkdir build
-#cd build
-#cmake -D CMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ .. -DBUILD_SHARED_LIBS=OFF
-#make -j
-#cd ..
-#cp -r ./build/lib/* ./lib/
-#mv ./lib/libassimp.a ./lib/libassimpWin.a
-#cp ./build/include/assimp/config.h ./include/assimp/
